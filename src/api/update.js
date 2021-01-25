46a96d5f3c4d7c7f4f410dc6012b1fc8dd8df551
Mateusz Choma <kontakt@coderoad.pl>
2021-01-25 08:53:49 +0100

    `update` API function added

--- src/api/update.js
+++ src/api/update.js
@@ -1,1 +1,11 @@
-// @TODO task change isCompleted
+import { makeApiUrl } from './makeApiUrl'
+
+export const update = (listKey, taskKey, dataToUpdate) => {
+    const apiUrl = makeApiUrl(`${listKey}/${taskKey}`)
+
+    return fetch(apiUrl, {
+        method: 'PATCH',
+        body: JSON.stringify(dataToUpdate)
+    })
+        .then((response) => response.json())
+}
